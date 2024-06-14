962e0886c3dcacbe9ad8ba65007ff8879f747d28
Kundan Choudhary <kundankumar02002@gmail.com>
2024-06-14 20:27:49 +0530

    final version

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
+import requests
 from flask import Flask, render_template
+from post import Post
+
+post_objects=[]
+
+posts =requests.get("https://api.npoint.io/cec3f87575a5af5bca03").json()
+for post in posts:
+    post_obj=Post(post["id"],post["title"],post["subtitle"],post["body"], post["image_url"])
+    post_objects.append(post_obj)
 
 app=Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html",post=posts)
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
+
+@app.route("/post/<int:blog_id>")
+def post(blog_id):
+    for post in post_objects:
+        if post.id==blog_id:
+            return render_template("post.html",post=post)
+
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
---
+++ post.py
@@ -0,0 +1,7 @@
+class Post():
+    def __init__(self,id,title,subtitle,body,image_url):
+        self.id=id
+        self.title=title
+        self.subtitle=subtitle
+        self.body=body
+        self.image_url=image_url
