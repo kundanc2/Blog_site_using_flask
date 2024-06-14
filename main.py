b3d4d4bde804c39f6e5c093ebf4da995a27bb676
Kundan Choudhary <kundankumar02002@gmail.com>
2024-06-15 02:21:55 +0530

    Working contact form

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from post import Post
+import smtplib
+
+
+
+OWN_EMAIL = "YOUR OWN EMAIL ADDRESS"
+OWN_PASSWORD = "YOUR EMAIL ADDRESS PASSWORD"
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
 
 post_objects=[]
 
@@ -29,6 +43,14 @@ def post(blog_id):
         if post.id==blog_id:
             return render_template("post.html",post=post)
 
-
+@app.route("/form-entry",methods=["POST","GET"])
+def contact_form():
+    if request.method == "POST":
+        
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+        return render_template("contact.html",success=1)
+    return render_template("contact.html",success=0)
 if __name__=="__main__":
     app.run(debug=True)
